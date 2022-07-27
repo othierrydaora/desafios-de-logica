d062d37efc5dee1cb2daf09524a371e4c8785c3a
Thierry <91567505+othierrydaora@users.noreply.github.com>
2022-07-27 09:48:27 -0300

    Desafio sql

---
+++ outros/sql/desafio.sql
@@ -0,0 +1,21 @@
+use desafiodb;
+show tables;
+select * from tb_filme;
+select * from tb_ator;
+select * from tb_filme_ator;
+
+select * from tb_filme where nr_ano = 2016;
+select * from tb_filme where nr_duracao > 180;
+select * from tb_filme where ds_pais = 'Brazil';
+select * from tb_filme where vl_avaliacao_imdb < 5;
+select * from tb_filme where ds_generos like '%Comedy%' and nr_ano = 2016;
+select * from tb_filme where nr_ano between 1990 and 2000;
+select * from tb_filme where nm_filme like '%Batman%';
+select * from tb_filme where nm_filme like 'Harry Potter%';
+select * from tb_filme where ds_generos = 'Comedy';
+select * from tb_filme where nm_filme = 'The%';
+select * from tb_filme where ds_linguagem = 'French';
+select * from tb_filme where ds_linguagem = 'French' and ds_linguagem = 'English';
+select * from tb_filme where ds_generos like '%Musical%' and nm_filme like '%Step%';
+select * from tb_filme where ds_generos = 'Romance' or ds_generos = 'Horror';
+select * from tb_filme where nr_ano < 2000 and 
