61d72e390a34600027d986303b4d859b02d37521
othierrydaora <thierryalefh77@gmail.com>
2022-07-28 16:31:30 -0300

    Atualização do palíndromo

--- outros/c/programas/palindromo.c
+++ outros/c/programas/palindromo.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
-int palindromo(char string[20])
+int palindromo(char *string)
 {
-  int limit = 0;
-  for (int i = 0; string[i] != '\0'; i++)
-    limit++;
-  for (int j = 0; j < limit; j++) {
-    if (string[j] != string[limit - j - 1])
-      return 1;
+  const int comp = strlen(string);
+  for (int j = 0; j < comp; j++) {
+    if (string[j] != string[comp - j - 1])
+      return 0;
   }
-  return 0;
+  return 1;
 }
 
 int main()
 {
   char palavra[20];
-  puts("Programa que verifica se uma string é um palíndromo");
+  puts("Programa que verifica se uma palavra é um palíndromo");
   puts("Insira a palavra: ");
   scanf("%s", palavra);
   int result = palindromo(palavra);
   if (result)
-  puts("Não é um palíndromo");
+    puts("É um palíndromo");
   else
-  puts("É um palíndromo");
+    puts("Não é um palíndromo");
   return 0;
 }
